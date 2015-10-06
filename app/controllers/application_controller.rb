ecc082a17c845245b2318daaa4ae43ed3b4f062a
USGrant63 <USGrant63@gmail.com>
2015-10-05 22:20:57 -0400

    Improve the README file

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+#randomchangeshalevaskedfor
+
   def hello 
-  	render text: "hello, world!"
+  	render text: "¡hola, mundo!"
   end
+
+  def goodbye
+  	render text: "goodbye, world."
+  end
+  
 end
 
